254fe473bf3b74a468efcf4a66104cb7c0ec4e03
Felipe Fontoura <felipelfontoura@gmail.com>
2021-11-10 00:52:55 -0300

    RegistrosV2

--- Regsitros.c
+++ Regsitros.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
+typedef struct
+{
+    char nome[100];
+    int idade, status;
+    float salario;
+} t_pessoa;
 
 int main()
 {
-    typedef struct
-    {
-        char nome[100];
-        int idade, status;
-        float salario;
-    } t_pessoa;
+    t_pessoa *p = 0;
 
+    printf("\n", ler_pessoa(p));
     return 0;
+}
+
+t_pessoa ler_pessoa(t_pessoa *p)
+{
+    
 }
