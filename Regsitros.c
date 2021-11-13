555996c5c3be6125462893f17de0818b5aa8f9d4
Felipe Fontoura <felipelfontoura@gmail.com>
2021-11-13 11:29:59 -0300

    RegistrosV4

--- Regsitros.c
+++ Regsitros.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*1. Defina em C um novo tipo denominado t_pessoa que contenha as seguintes características:
 • nome: String (tamanho 100),
@@ -6,7 +7,7 @@
 • salário: real, e
 • status: inteiro (0 - indica inativo; e 1 - indica ativo)
 */
-typedef struct
+typedef struct //Definição da estrutura "t_pessoa"
 {
     char nome[100];
     int idade, status;
@@ -16,7 +17,7 @@ typedef struct
 /*2. Implemente a função ler_pessoa(t_pessoa *p), que permite ler todos os dados relativos a uma
 determinada pessoa. Considere que toda nova pessoa é inicializada com status 1 - ativo.*/
 
-void ler_pessoa(t_pessoa *p)
+void ler_pessoa(t_pessoa *p) //Procediemnto para inserir os dados da pessoa
 {
     printf("Insira o nome da pessoa:");
     fgets(p->nome, 100, stdin);
@@ -33,20 +34,10 @@ void ler_pessoa(t_pessoa *p)
 /*3. Implemente a função mostrar_pessoa(t_pessoa *p), que permite mostrar todos os dados relativos a
 uma determinada pessoa.*/
 
-void mostrar_pessoa(t_pessoa *p)
+void mostrar_pessoa(t_pessoa *p) //Procediemnto para imprimir os dados da pessoa
 {
     printf("\nPessoa 1:\nNome: %sIdade: %d\nSalario: %.2f\nStatus: ", p->nome, p->idade, p->salario);
     if (p->status == 1) printf("Ativo\n"); else printf("Inativo\n");
-}
-
-int main()
-{
-    t_pessoa pessoa;
-
-    ler_pessoa(&pessoa);
-    mostrar_pessoa(&pessoa);
-    
-    return 0;
 }
 
 /*4. Considerando uma lista vazia de 10 registros de pessoas t_pessoa pessoas[10], implemente uma
@@ -63,19 +54,109 @@ iii) 3. Pesquisar por Status
 iv) 0. Voltar
 f) 6. Sair*/
 
-int appPessoas(t_pessoa *p)
+void appPessoas(t_pessoa *p) //Função para fazer o menu de interação
 {
-    t_pessoa pessoas[10];
-    return 0;
+    int input;
+
+    while(input != 6)
+    {
+        system("cls");
+
+        //Inserir Registro
+        printf("1. Inserir Registro\n");
+
+        //Alterar Registro
+        printf("2. Alterar registro\n");
+
+        //Apagar Registro
+        printf("3. Apagar registro\n");
+
+        //Listar Registros
+        printf("4. Listar registros\n");
+
+        //Pesquisas
+        printf("5. Pesquisas\n");
+
+        //Sair
+        printf("6. Sair\n");
+    
+        printf("Escolha uma das opcoes: ");
+        scanf("%d%*c", &input);
+
+        switch(input)
+        {
+            case 1:
+                system("cls");
+                inserir_registro();
+                break;
+            case 2:
+                system("cls");
+                alterar_registro();
+                break;
+            case 3:
+                system("cls");
+                apagar_registro();
+                break;
+            case 4:
+                system("cls");
+                listar_registros();
+                break;
+            case 5:
+                system("cls");
+                pesquisas();
+                break;
+            default:
+                printf("Apenas números de 1 a 6, por favor!");
+        }
+    }
 }
 
-// int main()
-// {
-//     t_pessoa pessoa;
+void inserir_registro(t_pessoa *p)
+{
+    printf("Aonde deseja inserir este registro? ");
 
-//     ler_pessoa(pessoa);
-//     mostrar_pessoa(pessoa);
-//     appPessoas(pessoa);
+    printf("Insira o nome da pessoa:");
+    fgets(p->nome, 100, stdin);
+
+    printf("Agora sua idade: ");
+    scanf("%d", &p->idade);
+
+    printf("E seu salario: ");
+    scanf("%f", &p->salario);
+
+    printf("Qual o status atual desta pessoa? (0 - Inativa | 1 - Ativa) ");
+    scanf("%d", &p->status);
+}
+
+void alterar_registro()
+{
+
+}
+
+void apagar_registro()
+{
+
+}
+
+void listar_registros()
+{
+
+}
+
+void pesquisas()
+{
+
+}
+
+int main() //MAIN final com todo o programa
+{
+    // t_pessoa pessoa;
+    t_pessoa pessoas[10];
+    int n = 2;
+
+    // ler_pessoa(&pessoa);
+    // mostrar_pessoa(&pessoa);
+    appPessoas(&pessoas[n]);
     
-//     return 0;
-// }
+    return 0;
+}
