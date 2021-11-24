c988cc202f148f9314770197ada9327ab67cb694
Felipe Fontoura <felipelfontoura@gmail.com>
2021-11-24 11:57:41 -0300

    P2

--- 2112130011_arquivos_FelipeLGCdaFontoura.c
+++ 2112130011_arquivos_FelipeLGCdaFontoura.c
@@ -13,18 +13,21 @@ linha por linha, e imprima na tela apenas se as notas forem maior ou igual a 7.*
 
 void calcular_media_notas(int n, int notas[n])
 {
-  FILE *fp = fopen("C:\\Programação\\C\\C Scripts\\notas.txt", "r");
+  int media;
+  FILE *fp = fopen(".\\notas.txt", "r");
 
-  if(fp != NULL)
+  if(fp == NULL)
+		printf("Nao foi possivel abrir o arquivo\n");
+	else
   {
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i <= n; i++)
     {
-      for (int j = 0; scanf("%d %d %d %d", notas[j]) != '\n'; j++;)
+      for (int j = 0; fscanf(fp, "%d %d %d %d", &notas[0], &notas[1], &notas[2], &notas[3]) != '\n'; j++)
       {
-        media + notas[j];
+        media = media + notas[j];
       }
-      if((media / n)) >= 7)
-        printf("Notas maiores que 7:\n%d", media / n);
+      if((media / n) >= 7)
+        printf("Notas maiores que 7:\n%d", (media / n));
     }
     fclose(fp);
   }
@@ -33,27 +36,27 @@ void calcular_media_notas(int n, int notas[n])
 /*(1 ponto) b) Realize a leitura dos valores do vetor de datas e imprima cada linha no arquivo
 "./datas.txt" os valores no seguinte formato: dd/mm/aaaa */
 
-// ler_imprimir_datas()
-// {
-//     FILE *fp = fopen("./datas.txt", "w");
+void ler_imprimir_datas(int n, t_data datas[n])
+{
+    FILE *fp = fopen("./datas.txt", "w");
 
-//   if(fp != NULL) {
-//     // operações de leitura e escrita
-//     for(int i = 0; i < n; i++) {
-//       fprintf(fp, "%d\\%d\\%d\n",
-//           datas[i]->t_data.dia,datas[i]->t_data.mes,datas[i]->t_data.ano);
-//     }
-//     fclose(fp);
-//   }
-// }
+  if(fp != NULL)
+  {
+    for(int i = 0; i < n; i++)
+    {
+      fprintf(fp, "%d\\%d\\%d\n", datas[i].dia, datas[i].mes, datas[i].ano);
+    }
+    fclose(fp);
+  }
+}
 
 int main()
 {
   t_data datas[4];
-
 	int notas[4];
   
 	calcular_media_notas(4, notas);
+  ler_imprimir_datas(4, datas);
   return 0;
 }
 
